832d5a551086461c376f37b4da64e598e2cb15a6
Michael M. Wasser <untoldone@users.noreply.github.com>
2012-02-04 01:47:30 -0800

    cleanup

--- lib/raveld_aws/request.rb
+++ lib/raveld_aws/request.rb
@@ -12,60 +12,40 @@ module RaveldAws
       parameters['SignatureVersion'] = '2'
       parameters['SignatureMethod'] = 'HmacSHA256'
       parameters['Timestamp'] = utc_iso8601(Time.now)
-      parameters['Signature'] = calculate_signature("GET", 
-                                                    host, 
-                                                    parameters, 
-                                                    aws_secret_key)
+      parameters['Signature'] = calculate_signature("GET", host, parameters, aws_secret_key)
       query = parameters.keys.map do |key|
         "#{amz_escape(key)}=#{amz_escape(parameters[key])}"
       end.join('&')
       @uri = "https://#{host}/?#{query}"
     end
-    
-    # Can be called with a block if 'custom' error handling is needed.
-    # The block will be called with the response hash if an error occurs.
-    # If no block is given on an error, a BrainstemException will be raised.
-    #
-    # For example, the structure of the AMZN error response for EC2 is different
-    # than that of EMR.
+
     def execute
       puri = URI.parse(@uri)
       http = Net::HTTP.new(puri.host, puri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
       request = Net::HTTP::Get.new(puri.request_uri) 
       response = http.request(request)
-      
       if (!response || (response.code != '200'))
-        # failure
         if response && !response.body.nil?
-          # grab the AMZN error message and pass it along
           raise xml_to_hash(response.body)
         else
-          # no error info to be found
           raise 'unknown'
         end
       else
-        # success
         return xml_to_hash(response.body)
       end
     end
     
 private
     def xml_to_hash(xml)
-      begin
-        XML.default_load_external_dtd = false
-        XML.default_pedantic_parser = true
-        result = XML::Parser.string(xml).parse
-        return { result.root.name.to_s => xml_node_to_hash(result.root)}
-      rescue Exception => e
-        # raise your custom exception here
-      end
+      XML.default_load_external_dtd = false
+      XML.default_pedantic_parser = true
+      result = XML::Parser.string(xml).parse
+      return { result.root.name.to_s => xml_node_to_hash(result.root)}
     end
     
     def xml_node_to_hash(node)
-      # If we are at the root of the document, start the hash
       if node.element?
         if node.children?
           result_hash = {}
@@ -101,7 +81,6 @@ private
       time.utc.strftime("%Y-%m-%dT%H:%M:%S.000Z")
     end
 
-    # Escape a string according to Amazon rules
     def amz_escape(param)
       CGI.escape(param.to_s).gsub("+", "%20").gsub("%7E", "~")
     end
